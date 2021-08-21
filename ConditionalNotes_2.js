1eb5da39a08d6aa4933ccc6642d7b37604edc9ec
Harry <harisathya1995@gmail.com>
2021-08-21 15:54:15 +0530

    conditionalAndLoopingCompleted

--- ConditionalNotes_2.js
+++ ConditionalNotes_2.js
@@ -22,4 +22,20 @@ v-else="condition"
 
 V-show: - will element will hide the element from DOM by settin the display property to none - display:none
 v-show = display:none 
+
+
+Looping:
+Looping on Array:
+Format: v-for="(iterator,idx) in Array" v-bind:key="idx"
+<div v-for="(fruit, idx) in fruits" v-bind:key="idx">
+  <h1>{{ fruit }}</h1>
+  {{ idx }}
+</div>
+This will create No of above markup based on the array length. Eg:array has 5 items -> UI will have 5 Div
+
+Looping on Object: Looping on object means getting key value pairs:
+Format: v-for="(value, key, index) in ObjectName" v-bind:key="index"
+ <div v-for="(value, key, index) in person" v-bind:key="index">
+    {{ key }} : {{ value }} ({{ index }})
+</div>
 */
--- src/components/Condiitionals/Conditional.vue
+++ src/components/Condiitionals/Conditional.vue
@@ -27,6 +27,61 @@
     <div v-show="show">
       <p>Using v-show</p>
     </div>
+
+    <!-- Conditional rendering Looping -->
+    <div class="looping">
+      <h2>Conditional Looping</h2>
+      <ul>
+        <li v-for="(fruit, idx) in fruits" v-bind:key="idx">
+          {{ idx + 1 }} - {{ fruit }}
+        </li>
+      </ul>
+      <!-- Grouping Element -->
+      <div v-for="(fruit, idx) in fruits" v-bind:key="idx">
+        <h1>{{ fruit }}</h1>
+        {{ idx }}
+      </div>
+      <hr />
+      <!-- Looping via Object  -->
+      <ul>
+        <li v-for="(person, idx) in persons" v-bind:key="idx">
+          <!-- Looping in object - extracting key value pairs -->
+          <div v-for="(value, key, index) in person" v-bind:key="index">
+            {{ key }} : {{ value }} ({{ index }})
+          </div>
+        </li>
+      </ul>
+      <!-- Looping Via Numbers -->
+      <ul>
+        <span>prev</span>
+        <span class="numBox" v-for="n in 5" v-bind:key="n">{{ n }}</span>
+        <span>next</span>
+      </ul>
+
+      <!-- Deleting Item - My Try - Tracking of elements -->
+      <div class="wrapper">
+        <h2>Keeping Track of Items</h2>
+        <div class="fruits">
+          <ul>
+            <li v-for="(fruit, idx) in fruits" :key="idx">
+              {{ fruit }}
+              <span class="deleteBtn" @click="deleteItem(idx, $event)">X</span>
+            </li>
+          </ul>
+          <!-- Removed Fruits Tracked Here -->
+          <ul class="removedFruits">
+            <li v-for="(fruit, idx) in trash" :key="idx">
+              Removed Fruit - {{ fruit }}
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Single Object -->
+      <div v-for="(value, key, idx) in user" :key="idx">
+        <h1>{{ key.toUpperCase() }}: {{ value }}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,7 +92,30 @@ export default {
     return {
       show: true,
       num: 0,
+      fruits: ['apple', 'mongo', 'berry', 'orange', 'grapes'],
+      persons: [
+        { name: 'John', age: 25, role: 'developer' },
+        { name: 'Smith', age: 26, role: 'designer' },
+      ],
+      trash: [],
+      user: {
+        name: 'harry',
+        age: 26,
+        role: 'dev',
+      },
     };
+  },
+
+  methods: {
+    deleteItem: function(idx, e) {
+      // Removed Item
+      let removed = this.fruits.filter((item, index) => idx === index);
+      console.log(removed, 'removed');
+
+      this.fruits = this.fruits.filter((item, i) => idx !== i);
+      this.trash.push(...removed);
+      console.log(this.trash, 'trash');
+    },
   },
 };
 </script>
@@ -45,5 +123,29 @@ export default {
 <style lang="scss" scoped>
 #root {
   margin: 2rem 0;
+
+  .numBox {
+    padding: 0.5rem;
+    margin: 1rem 0.25rem;
+    background: blue;
+    color: #fff;
+  }
+
+  .fruits {
+    display: flex;
+    justify-content: space-evenly;
+
+    .removedFruits {
+      padding: 0.5rem;
+      border: 1px solid red;
+      min-width: 300px;
+      min-height: 10px;
+    }
+  }
+
+  .deleteBtn {
+    padding: 0.5rem;
+    cursor: pointer;
+  }
 }
 </style>
